83485175bdac7394dc1274d08c255b8169ee29b6
sherlock213 <windhack0@gmail.com>
2017-07-16 15:27:32 +0700

    Create delc.sh

---
+++ delc.sh
@@ -0,0 +1,23 @@
+chmod -x menu
+chmod -x usernew
+chmod -x trial
+chmod -x hapus
+chmod -x cek
+chmod -x member
+chmod -x resvis
+chmod -x speedtest
+chmod -x info
+chmod -x about
+#del
+rm menu
+rm usernew
+rm trial
+rm hapus
+rm cek
+rm member
+rm resvis
+rm speedtest
+rm info
+rm about
+#fnsh
+rm -f /root/delc.sh
